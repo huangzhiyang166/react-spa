6ae6e67a75b00e21e359b32cfac26d6e2b852b7f
huangzhiyang <348845730@qq.com>
2019-07-17 15:28:54 +0800

    添加code split / dynamic imports及路由动态加载功能

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -9,13 +9,13 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const config = merge(baseWebpackConfig("production"),{
     mode: 'production',
     optimization: {
-		minimizer : [
-			new UglifyJsPlugin({
-				cache: true,
-				parallel: true,
-				sourceMap: false
-			}),
-		],
+		// minimizer : [
+		// 	new UglifyJsPlugin({
+		// 		cache: true,
+		// 		parallel: true,
+		// 		sourceMap: false
+		// 	}),
+		// ],
         splitChunks: {
             // （默认值3）入口点上的最大并行请求数
 			minChunks: 1,
--- src/view/app/App.js
+++ src/view/app/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import { Provider } from "react-redux";
 import { hot } from "react-hot-loader";
 import {login,getMemberInfo} from "@/api";
-console.log(process.env);
 let App = (props) => {
+    function onClick(e){
+        import(/* webpackChunkName:'aa' */ "./aa").then((module) => {
+            const aa = module.default;
+            aa();
+        })
+        aa();
+    }
     return (
         <Provider store={props.store}>
-            <p className="title">看我实现热更新!!!</p>
+            <p onClick={onClick} className="title">看我实现热更新!!!</p>
         </Provider>
     );
 };
 // 增加热更新
-if (module.hot) {
+if(module.hot){
     App = hot(module)(App);
 }
 
