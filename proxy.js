c5e739390689cd0030d20bd40fe3bdd54a21be39
huangzhiyang <348845730@qq.com>
2019-07-17 10:38:33 +0800

    配置webpack dev server接口代理，处理代理过程中cookie丢失问题

--- proxy.js
+++ proxy.js
@@ -1,16 +1,17 @@
 function onProxyRes(proxyRes,req,res){
-    var cookies = proxyRes.headers['set-cookie'];
-    var cookieRegex = /domain=\.12301\.local/i;
-    //修改cookie Path
-    if(cookies){
-        var newCookie = cookies.map((cookie) => {
-            if (cookieRegex.test(cookie)) return cookie.replace(cookieRegex, 'domain=localhost');
-            return cookie;
-        });
-        //修改cookie path
-        delete proxyRes.headers['set-cookie'];
-        proxyRes.headers['set-cookie'] = newCookie;
-    }
+    //直接用cookieDomainRewrite配置一下就可以了，不需要再用以下代码
+    // var cookies = proxyRes.headers['set-cookie'];
+    // var cookieRegex = /domain=\.12301\.local/i;
+    // //修改cookie Path
+    // if(cookies){
+    //     var newCookie = cookies.map((cookie) => {
+    //         if (cookieRegex.test(cookie)) return cookie.replace(cookieRegex, 'domain=localhost');
+    //         return cookie;
+    //     });
+    //     //修改cookie path
+    //     delete proxyRes.headers['set-cookie'];
+    //     proxyRes.headers['set-cookie'] = newCookie;
+    // }
 }
 function onProxyReq(proxyReq,req,res){
 
@@ -23,6 +24,9 @@ const proxy = {
         changeOrigin: true,   // 是否跨域
         onProxyRes,
         onProxyReq,
+        cookieDomainRewrite : {
+            ".12301.local" : "localhost"
+        }
     },
     "/dlogin.php" : {
         target : "http://my.12301.local",
@@ -30,7 +34,10 @@ const proxy = {
         changeOrigin: true,  
         onProxyRes,
         onProxyReq,
-    }
+        cookieDomainRewrite : {
+            ".12301.local" : "localhost"
+        }
+    },
 }
 
 module.exports = proxy;
